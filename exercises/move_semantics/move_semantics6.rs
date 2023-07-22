e844f4372b505f5cd0ac481d136fd33ad337b390
Prashant Iyer <prashantiyer2019@gmail.com>
2023-07-23 00:51:49 +0530

    This is till move_semantics

--- exercises/move_semantics/move_semantics6.rs
+++ exercises/move_semantics/move_semantics6.rs
@@ -10,19 +10,19 @@
 fn main() {
     let data = "Rust is great!".to_string();
 
-    get_char(data);
+    get_char(&data); // This is done beacuse as the problem statement said it should not take the ownership, That's the reason of referencing here in this fn
 
-    string_uppercase(&data);
+    string_uppercase(data);
 }
 
 // Should not take ownership
-fn get_char(data: String) -> char {
+fn get_char(data: &String) -> char {
     data.chars().last().unwrap()
 }
 
 // Should take ownership
-fn string_uppercase(mut data: &String) {
-    data = &data.to_uppercase();
-
+fn string_uppercase(mut data: String) {        // Refrencing removed from here as It has to take ownership
+    data = data.to_uppercase();
+  
     println!("{}", data);
 }
